6bd5fce90a38faef95f6612c008723295b665eec
cmcnr82 <cmcnr82@gmail.com>
2025-02-08 18:00:29 +0300

    yml for dev is added to config server.

--- services/gateway/src/main/java/com/portfoliotracker/gateway/GatewayApplication.java
+++ services/gateway/src/main/java/com/portfoliotracker/gateway/GatewayApplication.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpMethod;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-//@OpenAPIDefinition(info = @Info(title = "API Gateway", version = "1.0", description = "Documentation API Gateway v1.0"))
 public class GatewayApplication {
 
 	public static void main(String[] args) {
