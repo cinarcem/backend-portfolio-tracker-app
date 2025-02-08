58ee406dd9a40f2672b319f18920937b35bd3d4e
cmcnr82 <cmcnr82@gmail.com>
2025-02-08 22:37:41 +0300

    market data dev yml updated.

--- services/marketdata/src/main/java/com/portfoliotracker/marketdata/config/OpenApiConfigs.java
+++ services/marketdata/src/main/java/com/portfoliotracker/marketdata/config/OpenApiConfigs.java
@@ -20,7 +20,7 @@ public class OpenApiConfigs {
             @Value("${openapi.service.version}") String serviceVersion,
             @Value("${openapi.service.url}") String url) {
         return new OpenAPI()
-                .servers(List.of(new Server().url("https://gateway-portfolio-tracker-app-dev.up.railway.app")))
+                .servers(List.of(new Server().url(url)))
                 .info(new Info().title(serviceTitle).version(serviceVersion));
     }
 
