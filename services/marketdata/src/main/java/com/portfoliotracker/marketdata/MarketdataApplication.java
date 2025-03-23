82c1dd5342f52acdf1ca66fe42f32db044255910
cmcnr82 <cmcnr82@gmail.com>
2025-03-23 16:45:53 +0300

    SystemMetricsAutoConfiguration.class removed at boot due to lightweigt dockerfile can not handled it.

--- services/marketdata/src/main/java/com/portfoliotracker/marketdata/MarketdataApplication.java
+++ services/marketdata/src/main/java/com/portfoliotracker/marketdata/MarketdataApplication.java
@@ -1,11 +1,12 @@
 package com.portfoliotracker.marketdata;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SystemMetricsAutoConfiguration.class})
 @EnableDiscoveryClient
 @EnableScheduling
 public class MarketdataApplication {
--- services/portfolioservice/src/main/java/com/portfoliotracker/portfolioservice/PortfolioserviceApplication.java
+++ services/portfolioservice/src/main/java/com/portfoliotracker/portfolioservice/PortfolioserviceApplication.java
@@ -1,10 +1,11 @@
 package com.portfoliotracker.portfolioservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SystemMetricsAutoConfiguration.class})
 @EnableDiscoveryClient
 public class PortfolioserviceApplication {
 
